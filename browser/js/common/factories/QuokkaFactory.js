ac61dea88917d982ff41a7154c42e34c2e37e55b
Jennifer <jenncui@gmail.com>
2015-07-22 17:15:41 -0400

    gave quincy a background

--- browser/js/common/factories/QuokkaFactory.js
+++ browser/js/common/factories/QuokkaFactory.js
@@ -6,20 +6,20 @@ app.factory('QuokkaFactory', function() {
                 if (event.keyCode == 37) { //left
                     console.log('hit left button, yo');
                     $('img').animate({
-                        'left': '-=100'
+                        'left': '-=200'
                     });
                 } else if (event.keyCode == 38) { //up
                     $('img').animate({
-                        'top': '-=100'
+                        'top': '-=200'
                     })
                 } else if (event.keyCode == 39) { //right
                     console.log('hit right button, yo');
                     $('img').animate({
-                        'left': '+=100'
+                        'left': '+=200'
                     })
                 } else if (event.keyCode == 40) { //down
                     $('img').animate({
-                        'top': '+=100'
+                        'top': '+=200'
                     })
                 }
             })
